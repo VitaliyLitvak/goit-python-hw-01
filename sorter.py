7ed31097548330b51c1c815a9eb4adf00d26f3f8
VitaliyLitvak <vitaliy.litvak@gmail.com>
2023-05-24 16:34:24 +0200

    changed to pathlib instead of OS

--- sorter.py
+++ sorter.py
@@ -1,4 +1,6 @@
-import os, shutil, re
+import shutil, re
+from pathlib import Path
+
 
 def normalize(file_name):
     CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
@@ -12,59 +14,54 @@ def normalize(file_name):
     file_name = ''.join(TRANS.get(ord(ch), ch) for ch in file_name)
     return file_name
 
-def proccessing(path):
-    items = os.listdir(path)
-    for item in items:
-        item_path = os.path.join(path, item)
-        if os.path.isfile(item_path):
-            file_name, file_ext = os.path.splitext(item) 
-            file_ext = file_ext.lower()
-            images = ('.jpeg', '.png', '.jpg', '.svg')
-            documents = ('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx')
-            audio = ('.mp3', '.ogg', '.wav', '.amr')
-            video = ('.avi', '.mp4', '.mov', '.mkv')
-            archives = ('.zip', '.gz', '.tar')
-            file_name = normalize(file_name)
-            norm_name = file_name + file_ext
-            if file_ext in images:
-                os.makedirs(os.path.join(path, 'images'), exist_ok=True)
-                os.rename(os.path.join(path, item), os.path.join(path, 'images', norm_name))
-            elif file_ext in documents:
-                os.makedirs(os.path.join(path, 'documents'), exist_ok=True)
-                os.rename(os.path.join(path, item), os.path.join(path, 'documents', norm_name)) 
-            elif file_ext in audio:
-                os.makedirs(os.path.join(path, 'audio'), exist_ok=True)
-                os.rename(os.path.join(path, item), os.path.join(path, 'audio', norm_name))     
-            elif file_ext in video:
-                os.makedirs(os.path.join(path, 'video'), exist_ok=True)
-                os.rename(os.path.join(path, item), os.path.join(path, 'video', norm_name))  
-            elif file_ext in archives:
-                try:
-                    os.makedirs(os.path.join(path, 'archives'), exist_ok=True)
-                    os.rename(os.path.join(path, item), os.path.join(path, 'archives', norm_name))
-                    shutil.unpack_archive(os.path.join(path, 'archives', norm_name), os.path.join(path, 'archives', file_name))
-                except shutil.ReadError:
-                    continue      
-            else:
-                continue
+
+def proccessing(item):
+    if item.is_file():
+        file_name, file_ext = item.stem, item.suffix 
+        file_ext = file_ext.lower()
+        images = ('.jpeg', '.png', '.jpg', '.svg')
+        documents = ('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx')
+        audio = ('.mp3', '.ogg', '.wav', '.amr')
+        video = ('.avi', '.mp4', '.mov', '.mkv')
+        archives = ('.zip', '.gz', '.tar')
+        file_name = normalize(file_name)
+        
+        norm_name = file_name + file_ext
+        if file_ext in images:
+            Path.mkdir(item.parent / 'images', exist_ok=True)
+            item.rename(item.parent / 'images' / norm_name)
+        elif file_ext in documents:
+            Path.mkdir(item.parent / 'documents', exist_ok=True)
+            item.rename(item.parent / 'documents' / norm_name) 
+        elif file_ext in audio:
+            Path.mkdir(item.parent / 'audio', exist_ok=True)
+            item.rename(item.parent / 'audio' / norm_name)    
+        elif file_ext in video:
+            Path.mkdir(item.parent / 'video', exist_ok=True)
+            item.rename(item.parent / 'video' / norm_name)  
+        elif file_ext in archives:
+            try:
+                Path.mkdir(item.parent / 'archives', exist_ok=True)
+                item.rename(item.parent / 'archives' / norm_name)
+                shutil.unpack_archive(Path(item.parent / 'archives' / norm_name), Path(item.parent / 'archives' / file_name))
+            except shutil.ReadError:
+                print("File can't be procceeded, please check if it's archive.")
             
   
 def sorter(path):
     ignore_list = ('archives', 'video', 'audio', 'documents', 'images')
-    items = os.listdir(path)
-    for item in items:
-        item_path = os.path.join(path, item)
-        item_path = os.path.normpath(item_path)
-        if os.path.isdir(item_path) and item not in ignore_list:
-            sorter(item_path)
-            if not os.listdir(item_path):
-                os.rmdir(item_path)
-        elif os.path.isfile(item_path):
-            proccessing(path)
+    for item in path.glob('*'):
+        if item.is_dir() and item.name not in ignore_list:
+            sorter(item)
+            if not list(item.glob('*')):
+                item.rmdir()
+        elif item.is_file():
+            print(f'file {item}')
+            print(item.parent)
+            proccessing(item)
             
-path = input('Ввведіть будь-ласка шлях до папки:')
-path = os.path.normpath(path)      
-
+            
+path = Path(input('Ввведіть будь-ласка шлях до папки: '))
 sorter(path)
 
 
